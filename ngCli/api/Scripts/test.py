c801a4bfde870d4dbd021a548b87fe212ee90c74
John Laureano <laureano.pedrojoao@gmail.com>
2019-04-29 11:37:06 -0300

    intervals implements WIP

--- ngCli/api/Scripts/test.py
+++ ngCli/api/Scripts/test.py
@@ -8,15 +8,12 @@ from utils import normalizer
 from pprint import pprint
 from collections import Counter
 def main():
-    pdb_coords = getDatas()
-    distances = allDistanceOfPairs(pdb_coords)
-    distance_freq = Counter(allDistanceOfPairs(pdb_coords))
-    pdb_coords = None
-    generateClass(distance_freq)
-
+    distances = allDistanceOfPairs(getDatas())
+    generateClass(distances)
 
 def generateClass(data):
     delta = max(data) - min(data)
+    print(delta/5)
     intervals = delta / 5 
     classes = dict()
     for x in data:
@@ -25,12 +22,13 @@ def generateClass(data):
             classes[interval] = list()
         classes[interval].append(x)
     print(classes)
+
 def getDatas():
     pdb_coords = list()
     pdb_coords.append(getCoord(parsePDB('1ZDD', header=True, secondary=True)))
-    # pdb_coords.append(getCoord(parsePDB('3NIR', header=True, secondary=True)))
-    # pdb_coords.append(getCoord(parsePDB('1N09', header=True, secondary=True)))
-    # pdb_coords.append(getCoord(parsePDB('1A5R', header=True, secondary=True)))
+    pdb_coords.append(getCoord(parsePDB('3NIR', header=True, secondary=True)))
+    pdb_coords.append(getCoord(parsePDB('1N09', header=True, secondary=True)))
+    pdb_coords.append(getCoord(parsePDB('1A5R', header=True, secondary=True)))
     # pdb_coords.append(getCoord(parsePDB('2NR2', header=True, secondary=True)))
     # pdb_coords.append(getCoord(parsePDB('2MOC', header=True, secondary=True)))
     # pdb_coords.append(getCoord(parsePDB('5LXY', header=True, secondary=True)))
@@ -46,7 +44,7 @@ def getDatas():
 def getCoord(pdb):
     hv = pdb[0].getHierView()
     coord_list = list()
-    for i, residue in enumerate(hv.iterResidues()):
+    for i,residue in enumerate(hv.iterResidues()):
             atom = residue.getAtom("CA")
             if not atom == None:
                 coord_list.append(atom.getCoords())
@@ -55,13 +53,13 @@ def getCoord(pdb):
     return coord_list
 
 def getDistance(point1,point2):
-    return hypot(point2[0] - point1[0], point2[1] - point1[1])
+    return trunc(hypot(point2[0] - point1[0], point2[1] - point1[1]))
 
 def allDistanceOfPairs(pdb):
-    pdb_coords = list()
+    pdb_coords = set()
     for x in range(0,len(pdb) ):
         for y in range(1,len(pdb[x]) ):
-            pdb_coords.append(getDistance(pdb[x][y],pdb[x][y-1]))
+            pdb_coords.add(getDistance(pdb[x][y],pdb[x][y-1]))
     return pdb_coords
 
 def normalizeAll(array_pdb):
