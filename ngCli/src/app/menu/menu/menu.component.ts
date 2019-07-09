641cb8682f56a6b878399b8740be443e97bc0ad6
johnnyloreano <laureano.pedrojoao@gmail.com>
2019-07-09 12:29:21 -0300

    small change Menu

--- ngCli/src/app/menu/menu/menu.component.ts
+++ ngCli/src/app/menu/menu/menu.component.ts
@@ -1,17 +1,13 @@
 import { Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  private pdbContent: any;
-  constructor( private _route: Router) {}
     /**
    * @ignore
    */
-  lastMenuV
   ngOnInit() {
     document.getElementById('menuLock').focus();
   }
