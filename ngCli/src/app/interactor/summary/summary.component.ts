6ce40ccc52ea23d14350e8cbc17beaf0d9cfa3e4
johnnyloreano <laureano.pedrojoao@gmail.com>
2019-07-15 15:14:25 -0300

    Changed Img Reference

--- ngCli/src/app/interactor/summary/summary.component.ts
+++ ngCli/src/app/interactor/summary/summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { DataService } from '../../core/data-service/data-service.service';
 import { Router } from "@angular/router";
 @Component({
